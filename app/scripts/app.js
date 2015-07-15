33dcfb9b9ce519b84060553b2d60e05d75107ee5
Jen Haines <motochick67@gmail.com>
2015-07-15 14:42:38 -0700

    Finished for now

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,7 @@ angular
 
   .constant('FIREBASE_URL', 'https://jennifer.firebaseio.com')
 
-  .factory('Room', function($firebase, $firebaseArray, $firebaseObject, FIREBASE_URL){
+  .factory('Room', function($firebase, $firebaseArray, $firebaseObject, FIREBASE_URL, Firebase){
     var ref = new Firebase(FIREBASE_URL + '/rooms');
     // var query = ref.orderByChild('created');
 
@@ -32,7 +32,7 @@ angular
     };
   })
 
-  .factory('Message', function($firebase, $firebaseArray, $firebaseObject, FIREBASE_URL){
+  .factory('Message', function($firebase, $firebaseArray, $firebaseObject, FIREBASE_URL, Firebase){
     var ref = new Firebase(FIREBASE_URL + '/messages');
     // var query = ref.orderByChild('created');
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
  angular
  .module('seederApp')
- .controller('SeedDataCtrl', function(Room, Message, $scope){
+ .controller('SeedDataCtrl', function(Room, Message, $scope, Firebase, chance){
    $scope.rooms= Room.all;
 
    $scope.options = [1,2,3,4,5,6,7,8,9,10];
@@ -21,44 +21,24 @@
      var timestamp = Firebase.ServerValue.TIMESTAMP;
      room.created = timestamp;
      Room.create(room);
-    };
+    }
     $scope.option ='';
-    $scope.status = num + " rooms created!"
+    $scope.status = num + ' rooms created!';
    };
 
-   $scope.createMsg = function(msgnum, rm ){
+   $scope.createMsg = function(msgnum, rm){
       for(var i=0; i<msgnum; i++){
-       var message = {username: '', body: '', roomId: rm.$id, created: ''};
+       var message = {username: '', content: '', roomId: rm.$id, sentAt: ''};
        message.username = chance.twitter();
-       message.body = chance.paragraph();
+       message.content = chance.paragraph();
        var timestamp = Firebase.ServerValue.TIMESTAMP;
-       message.created = timestamp;
+       message.sentAt = timestamp;
        Message.create(message);
         // console.log(msgnum + ' messages for ' + rm.name);
-      };
-      $scope.room ='';
+      }
+      $scope.msgnum ='';
+      $scope.msgselected ='';
     
-      $scope.status = msgnum + " messages created!";
-   };
-
-   // random number generator
-   var rndNumGen = function(start, finish){
-     return Math.floor((Math.random() * finish) + start);
-   };
-
- // used to create realistic seed data
-   var getRandomDate = function() {
-     var from = new Date(2015, 5, 1).getTime();
-     var to = new Date().getTime();
-     return new Date(from + Math.random() * (to - from)).getTime();
-   };
-
-   var getRandomStatus = function() {
-     var x = rndNumGen(0, 1);
-     if(x===0){
-       return 'active';
-     }else{
-       return 'complete';
-     }
+      $scope.status = msgnum + ' messages created!';
    };
  });
